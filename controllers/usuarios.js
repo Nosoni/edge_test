fd1d5ce8ec4d1a701be4157b8d663bc28f6cda5a
Franco <francisco.recalde.91@gmail.com>
2022-04-03 17:37:11 -0400

    usuarios filtrar / listar

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -22,9 +22,36 @@ module.exports = {
   },
   async filtrar(req, res) {
 
+    let filtrado = '';
+    Object.keys(req.body).map(key => {
+      filtrado += `${key}=${req.body[key]}&`
+    })
+
+    if (!filtrado) {
+      return res.status(400).send("Debe indicar un valor para filtrar")
+    }
+
+    var config = {
+      method: 'get',
+      servicio: `${servicio}?${filtrado.slice(0, -1)}`,
+    };
+
+    const usuarios = await peticion(config)
+      .then(response => response.map(usuario => {
+        delete usuario.password
+        return usuario
+      }))
+
+    return res.status(200).send(usuarios)
   },
   async listar(req, res) {
 
+    var config = {
+      method: 'get',
+      servicio: `${servicio}`,
+    };
+
+    return res.status(200).send(await peticion(config))
   },
   async editar(req, res) {
 
--- rutas/usuarios.js
+++ rutas/usuarios.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const usuarios = require("../controllers/usuarios")
 
-router.get(`/filtrar/:usuario`, usuarios.filtrar)
+router.get(`/filtrar`, usuarios.filtrar)
 router.get(`/listar`, usuarios.listar)
 router.put(`/editar`, usuarios.editar)
 router.put(`/eliminar/:id`, usuarios.eliminar)
